23940f5bca8bc0105e17dd31263ff7d0f7a564ac
aagnone3 <aagnone3@gatech.edu>
2015-06-06 21:59:31 -0400

    Initial updates to accommodate domain model design and registration functionality.

--- src/java/gatech/cs2340/team7/User.java
+++ src/java/gatech/cs2340/team7/User.java
@@ -5,60 +5,48 @@
  */
 package gatech.cs2340.team7;
 
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
 @ManagedBean(name = "user", eager = true)
 @SessionScoped
 public class User {
-    public String username;
-    public String password;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+    
+    private String realName;
+    public String newString;
+    @ManagedProperty(value="#{userAccount}")
+    private Account account;
+    private Profile myProfile;
+    //private List<Message> myMessages;
+    
+    public User() {
+        account = new Account();
     }
     
-    public String login() {
+    public User(Account account) {
+        this.account = account;
+    }
 
-        boolean userFound = username.equals("user");
-        boolean passwordMatched = password.equals("pass");
-        if (userFound && passwordMatched) {
-            return NavigationManager.success();
-        } else if (username.length() == 0) {
-             FacesContext.getCurrentInstance().addMessage(null,
-                    new FacesMessage(FacesMessage.SEVERITY_INFO, "Error", "Please enter username."));
-            return null;           
-        } else if (password.length() == 0) {
-             FacesContext.getCurrentInstance().addMessage(null,
-                    new FacesMessage(FacesMessage.SEVERITY_INFO, "Error", "Please enter password."));
-            return null;              
-        } else if (!userFound) {
-            FacesContext.getCurrentInstance().addMessage(null,
-                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Username not found!"));
-            return null;
-        } else {
-            FacesContext.getCurrentInstance().addMessage(null,
-                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Incorrect password!"));
-            return null;
-        }
+    public String getRealName() {
+        return this.realName;
+    }
+    
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+    
+    public void setAccount(Account myAccount) {
+        this.account = myAccount;
     }
     
     public String logout() {  
         FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
-        return "index?faces-redirect=true";
+        return NavigationManager.index;
     }  
 }
