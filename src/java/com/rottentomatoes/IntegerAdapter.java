e62131cf05e66ccf6a95adaf944b3419e5e60e53
aagnone3 <aagnone3@gatech.edu>
2015-06-17 19:13:48 -0400

    Added class that is registered with GSON to handle parsing of Integers. Specifically, this handles the empty string case, and returns -2 instead of throwing a NumberFormatException.

---
+++ src/java/com/rottentomatoes/IntegerAdapter.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rottentomatoes;
+
+import java.io.IOException;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.TypeAdapter;
+
+/**
+ *
+ * @author Anthony
+ */
+public class IntegerAdapter extends TypeAdapter<Integer> {
+    
+    @Override
+    public Integer read(JsonReader reader) throws IOException {
+        if (reader.peek() == JsonToken.NULL) {
+            reader.nextNull();
+            return null;
+        }
+        String val = reader.nextString();
+        try {
+            return Integer.parseInt(val);
+        } catch (NumberFormatException nfe) {
+            System.out.println("Caught number format exception, returning -2");
+            return -2;
+        }
+    }
+    
+    @Override
+    public void write(JsonWriter writer, Integer val) throws IOException {
+        if (val == null) {
+            writer.nullValue();
+            return;
+        }
+        writer.value(val);
+    }
+}
