c111813a17e12f78d7199a23d2503264dc2b100d
jspearman3 <spearmanjim@yahoo.com>
2015-07-07 21:19:35 -0400

    Merge origin/master

--- src/java/usermanagement/Account.java
+++ src/java/usermanagement/Account.java
@@ -10,56 +10,82 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
 /**
- * Represents a user's account-specific data
+ * Represents a user's account-specific data.
  * @author Anthony
  * @version 1.0
  */
 @ManagedBean(name = "account", eager = true)
 @SessionScoped
 public class Account implements Serializable {
-    
-    public static final int MAX_NUMBER_FLAGGED_COMMENTS = 2;
-    
-    private String username;
-    private boolean loggedIn;
-    private boolean locked;
-    private boolean banned;
-    private boolean admin;
-    private int numberFlaggedComments;
-    
     /**
-     * Constructs an empty account
+     * max number of flagged comments.
+     */
+    public static final int MAX_NUMBER_FLAGGED_COMMENTS = 2;
+
+    /**
+     * username associated with the account.
+     */
+    private String username;
+
+    /**
+     * boolean describing logged in status of account.
+     */
+    private boolean loggedIn;
+
+    /**
+     * boolean describing locked status of account.
+     */
+    private boolean locked;
+
+    /**
+     * boolean describing banned status of account.
+     */
+    private boolean banned;
+
+    /**
+     * boolean describing admin status of account.
+     */
+    private boolean admin;
+
+    /**
+     * number of flagged comments associated with the account.
+     */
+    private int numberFlaggedComments;
+
+    /**
+     * Constructs an empty account.
      */
     public Account() {
         this(null, false);
     }
-    
+
     /**
-     * Constructs an account with all relevant members belonging to an account
+     * Constructs an account with all relevant members belonging to an account.
      * @param username Username for the new account
      * @param admin Whether the account belongs to an admin
      */
     public Account(String username, boolean admin) {
-        this.username = username;
+        setUsername(username);
         this.loggedIn = false;
         this.locked = false;
         this.banned = false;
         this.admin  = admin;
         this.numberFlaggedComments = 0;
     }
-    
+
     /**
-     * Blocks the user from the account if necessary
-     * Else validates the login attempt
-     * @throws LockedAccountException if the account is locked
-     * @throws BannedAccountException if the account is locked
+     * Blocks the user from the account if necessary.
+     * Else validates the login attempt.
+     * @throws LockedAccountException if the account is locked.
+     * @throws BannedAccountException if the account is locked.
      */
-    public void login() throws LockedAccountException, BannedAccountException {
+    public final void login() throws LockedAccountException,
+            BannedAccountException {
         assertAccountStatus();
         if (banned) {
             System.out.print("This account is banned!");
             throw new BannedAccountException(
-                    "Account is banned by an administrator!");  
+                    "Account is banned by an administrator!");
         } else if (locked) {
             System.out.print("This account is locked!");
             throw new LockedAccountException(
@@ -67,26 +93,26 @@ public class Account implements Serializable {
         }
         loggedIn = true;
     }
-    
+
     /**
      * Assert that the account is locked if the number of flagged comments
      * exceeds the maximum allowed. Ideally, this method will be expanded
      * in future implementations, to account for different factors that could
      * affect the accounts locked/unlocked status
      */
-    public void assertAccountStatus() {
-        System.out.println("Account has " + this.numberFlaggedComments +
-                " flagged comments");
+    public final void assertAccountStatus() {
+        System.out.println("Account has " + this.numberFlaggedComments
+            + " flagged comments");
         if (numberFlaggedComments > MAX_NUMBER_FLAGGED_COMMENTS) {
             locked = true;
         }
     }
-    
+
     /**
-     * Checks and returns the status of the account
+     * Checks and returns the status of the account.
      * @return new String A string stating the status
      */
-    public String getStatus() {
+    public final String getStatus() {
         if (admin) {
             return "Administrator";
         } else if (banned) {
@@ -97,135 +123,135 @@ public class Account implements Serializable {
             return "Active";
         }
     }
-    
+
     /**
-     * Alters the status of the account to "locked"
+     * Alters the status of the account to "locked".
      */
-    public void lock() {
+    public final void lock() {
         locked = true;
         banned = false;
     }
-    
+
     /**
-     * Alters the status of the account to "unlocked"
+     * Alters the status of the account to "unlocked".
      */
-    public void unlock() {
+    public final void unlock() {
         locked = false;
         banned = false;
     }
-    
+
     /**
-     * Alters the status of the account to "banned"
+     * Alters the status of the account to "banned".
      */
-    public void ban() {
+    public final void ban() {
         banned = true;
         locked = true;
     }
-    
+
     /**
-     * Alters the status of the account to "unbanned"
+     * Alters the status of the account to "unbanned".
      */
-    public void unban() {
+    public final void unban() {
         banned = false;
     }
-    
+
     /**
-     * Getter method for the username of the account
+     * Getter method for the username of the account.
      * @return username The user who possesses the account
      */
-    public String getUsername() {
+    public final String getUsername() {
         return this.username;
     }
-    
+
     /**
-     * Getter method for login status
+     * Getter method for login status.
      * @return loggedIn Whether the account is logged in
      */
-    public boolean isLoggedIn() {
+    public final boolean isLoggedIn() {
         return loggedIn;
     }
-    
+
     /**
-     * Getter method for account locking status
+     * Getter method for account locking status.
      * @return locked Whether the account is locked
      */
-    public boolean isLocked() {
+    public final boolean isLocked() {
         return locked;
     }
-    
+
     /**
-     * Setter method for the username associated with the account
+     * Setter method for the username associated with the account.
      * @param username New username for the account
      */
-    public void setUsername(String username) {
+    public final void setUsername(String username) {
         this.username = username;
     }
-    
+
     /**
-     * Setter method for the login state of the account
+     * Setter method for the login state of the account.
      * @param loggedIn State of login to change to
      */
-    public void setLoggedIn(boolean loggedIn) {
+    public final void setLoggedIn(boolean loggedIn) {
         this.loggedIn = loggedIn;
     }
-    
+
     /**
-     * Setter method for the locked state of the account
+     * Setter method for the locked state of the account.
      * @param locked State of account lock to change to
      */
-    public void setLocked(boolean locked) {
+    public final void setLocked(boolean locked) {
         this.locked = locked;
     }
-    
+
     /**
-     * Determines if the account user is an admin
+     * Determines if the account user is an admin.
      * @return admin Whether the account belongs to an admin
      */
-    public boolean isAdmin() {
+    public final boolean isAdmin() {
         return admin;
     }
 
     /**
-     * Setter method for the admin state of the account
+     * Setter method for the admin state of the account.
      * @param admin State of user privileges to change to
      */
-    public void setAdmin(boolean admin) {
+    public final void setAdmin(boolean admin) {
         this.admin = admin;
     }
 
     /**
-     * Getter method for user banishment status
+     * Getter method for user banishment status.
      * @return banned Whether the user holding the account is banned
      */
-    public boolean isBanned() {
+    public final boolean isBanned() {
         return banned;
     }
 
     /**
-     * Setter method for user banishment status
+     * Setter method for user banishment status.
      * @param banned State of account ban to change to
      */
-    public void setBanned(boolean banned) {
+    public final void setBanned(boolean banned) {
         this.banned = banned;
     }
 
     /**
-     * Getter for the number of flagged comments the account has received
+     * Getter for the number of flagged comments the account has received.
      * @return Number of flagged comments associated with the account
      */
-    public int getNumberFlaggedComments() {
+    public final int getNumberFlaggedComments() {
         return numberFlaggedComments;
     }
 
     /**
-     * Setter for the number of flagged comments the account has received
+     * Setter for the number of flagged comments the account has received.
      * @param numberFlaggedComments Number of flagged comments
      *        associated with the account
      */
-    public void setNumberFlaggedComments(int numberFlaggedComments) {
+    public final void setNumberFlaggedComments(int numberFlaggedComments) {
         this.numberFlaggedComments = numberFlaggedComments;
     }
-    
-    
-    
+
+
+
 }
