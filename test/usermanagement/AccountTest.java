384a65457fc56f31238a2bcdb293ddfc446563b5
aagnone3 <aagnone3@gatech.edu>
2015-07-07 21:42:57 -0400

    Personal test of Account.login()

---
+++ test/usermanagement/AccountTest.java
@@ -0,0 +1,59 @@
+package usermanagement;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.assertFalse;
+
+import loginregistration.BannedAccountException;
+import loginregistration.LockedAccountException;
+
+/**
+ *
+ * @author Anthony
+ */
+public class AccountTest {
+
+    private Account account;
+
+    @Before
+    public void setup() {
+        account = new Account();
+        account.setUsername("user");
+    }
+
+    @Test
+    public final void testIdealLogin() 
+        throws LockedAccountException, BannedAccountException {
+        account.setNumberFlaggedComments(0);
+        account.login();
+        assertTrue("Account is not logged in!", account.isLoggedIn());
+        assertFalse("Account is incorrectly locked!", account.isLocked());
+        assertFalse("Account is incorrectly banned!", account.isBanned());
+    }
+    
+    @Test(expected = LockedAccountException.class)
+    public final void testAccountLockedLogin() 
+        throws LockedAccountException, BannedAccountException {
+        account.setLocked(true);
+        account.login();
+        assertFalse("Account is incorrectly logged in!", account.isLoggedIn());
+    }
+    
+    @Test(expected = BannedAccountException.class)
+    public final void testAccountBannedLogin()
+        throws LockedAccountException, BannedAccountException {
+        account.setBanned(true);
+        account.login();
+        assertFalse("Account is incorrectly logged in!", account.isLoggedIn());
+    }
+    
+    @Test(expected = LockedAccountException.class)
+    public final void testAccountFlagLockedLogin()
+        throws LockedAccountException, BannedAccountException {
+        account.setNumberFlaggedComments(3);
+        account.login();
+        assertFalse("Account is incorrectly logged in!", account.isLoggedIn());
+    }
+}
