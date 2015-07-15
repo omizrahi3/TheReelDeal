c798a7dc3ad9b9084f17c19ca493c6de1a9a75d4
kblask3 <kblask3@gatech.edu>
2015-07-15 11:58:39 -0400

    Completed junit test for checkNewUserRegistration() in Registration class.

--- test/loginregistration/RegistrationCheckNewUserTest.java
+++ test/loginregistration/RegistrationCheckNewUserTest.java
@@ -1,8 +1,10 @@
 package loginregistration;
 
+import java.util.Map;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import usermanagement.User;
 
 /**
  * This test is for the method checkNewUserRegistration() of the Registration
@@ -12,6 +14,8 @@ import static org.junit.Assert.assertEquals;
 public class RegistrationCheckNewUserTest {
 
     private Registration registration;
+    private Map<String, User> users;
+    private User aUser;
 
     @Before
     public void setUp() {
@@ -20,31 +24,56 @@ public class RegistrationCheckNewUserTest {
 
     @Test
     public final void testBranchCoverageNewUsername() {
-        assertEquals(false, registration.checkNewUserRegistration(null));
+        registration.setUsername("Calvin2000");
+        users.clear();
+        registration.setPassword("CALvin");
+        registration.setPasswordRepeat("CALvin");
+        assertEquals(true, registration.checkNewUserRegistration(users));
     }
 
     @Test
     public final void testBranchCoverageTakenUsername() {
+        registration.setUsername("Sam1989");
+        users.put("Sam1989", aUser);
+        registration.setPassword("moviesROCK");
+        registration.setPasswordRepeat("moviesROCK");
+        assertEquals(false, registration.checkNewUserRegistration(users));
         
     }
 
     @Test
     public final void testBranchCoverageNoUsername() {
-        
+        registration.setUsername(null);
+        users.clear();
+        registration.setPassword("password");
+        registration.setPasswordRepeat("password");
+        assertEquals(false, registration.checkNewUserRegistration(users));
     }
 
     @Test
     public final void testBranchCoveragePasswordsMatch() {
-        
+        registration.setUsername("BooRadley300");
+        users.clear();
+        registration.setPassword("ToKillaMockingBird");
+        registration.setPasswordRepeat("ToKillaMockingBird");
+        assertEquals(true, registration.checkNewUserRegistration(users));
     }
 
     @Test
     public final void testBranchCoveragePasswordsMismatch() {
-        
+        registration.setUsername("TrevorFr0g");
+        users.clear();
+        registration.setPassword("IluvHarryP0tter");
+        registration.setPasswordRepeat("iloveHarryPotter");
+        assertEquals(false, registration.checkNewUserRegistration(users));
     }
 
     @Test
     public final void testBranchCoverageNoPasswords() {
-        
+        registration.setUsername("userName100");
+        users.clear();
+        registration.setPassword(null);
+        registration.setPassword(null);
+        assertEquals(false, registration.checkNewUserRegistration(users));
     }
 }
